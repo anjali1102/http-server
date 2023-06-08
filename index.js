5b199f2e3974e7940ad2f1aeb4f78cb503559d2e
anjali1102 <ac.chauhan1102@gmail.com>
2023-06-08 19:07:07 +0530

    [feat]: learning http server

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const router = require("./UserRouter");
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api/v1", router);
+
+app.get("/", (req, res) => {
+  console.log(path.join("file", __dirname + "/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+// app.get("/test", (req, res) => {
+//   res.send(`
+//   <html><h2 style="color: pink;background: yellow;">testing :></h2></html>
+//   `);
+// });
+
+app.get("/api/v1/login", (req, res) => {
+  res.json({
+    name: "Anjali Chauhan",
+    email: "ac.chauhan1102@gmail.com",
+    password: "anjali1234",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
