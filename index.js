3cc0b43eea400918e3706f322e5216647853cc35
anjali1102 <ac.chauhan1102@gmail.com>
2023-06-08 20:58:38 +0530

    [fix]: removed unused route

--- index.js
+++ index.js
@@ -20,14 +20,6 @@ app.get("/", (req, res) => {
 //   `);
 // });
 
-app.get("/api/v1/login", (req, res) => {
-  res.json({
-    name: "Anjali Chauhan",
-    email: "ac.chauhan1102@gmail.com",
-    password: "anjali1234",
-  });
-});
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
