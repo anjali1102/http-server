5e69d165217095a9e5d07c9d99fe392a115d6076
anjali1102 <ac.chauhan1102@gmail.com>
2023-06-08 19:28:58 +0530

    [feat]: http server

--- UserController.js
+++ UserController.js
@@ -3,12 +3,13 @@ const loginUser = (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  res.json({
+  const result={
     success: "true",
     name: name,
     email: email,
     password: password,
-  });
+  };
+  res.send(`<h2 style="background:orange;color:red;align-items:center;height:100vh;display:flex;justify-content:center">${JSON.stringify(result)}</h2>`)
 };
 
 module.exports = loginUser;
