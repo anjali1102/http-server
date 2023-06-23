46eeb550a5505e5685a3e7d8e16f51457dfea448
anjali1102 <ac.chauhan1102@gmail.com>
2023-06-23 18:57:58 +0530

    Sending data in http request
    - Query Params in url eg http://localhost.com/?counter=20&counter2=30
    - Headers passed via postman
    - Body, which is not part of req, we use external lib body-parser
    - req.params.counter
    - req.headers.counter
    - req.body.counter (need body-parser lib)

--- UserRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const loginUser = require('./UserController')
-const router = express.Router();
-
-router.route("/login").post(loginUser);
-
-module.exports = router;
